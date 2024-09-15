a48baab37fb13c2472d3d567a12be3bfcc9208bf
OTTindia07 <meetzankhariyaking@gmail.com>
2024-09-15 22:37:47 +0530

    Update config_flow.py

--- custom_components/jr panel/config_flow.py
+++ custom_components/jr panel/config_flow.py
@@ -1,43 +1,62 @@
+"""Config flow for JR Touch Panel integration."""
+import logging
+from typing import Any
+
 import voluptuous as vol
+
 from homeassistant import config_entries
-from homeassistant.core import callback
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
+from homeassistant.data_entry_flow import FlowResult
 
-from .const import DOMAIN  # Make sure DOMAIN is defined in const.py
+from .const import DEFAULT_PORT, DOMAIN
+from .jr_accessory import JRAccessory
 
-@config_entries.HANDLERS.register(DOMAIN)
+_LOGGER = logging.getLogger(__name__)
+
 class JRPanelConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for JR Panel."""
+    """Handle a config flow for JR Touch Panel."""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
+
         if user_input is not None:
-            # TODO: Add validation for connection to the JR panel here
-            return self.async_create_entry(title=user_input["name"], data=user_input)
+            try:
+                accessory = JRAccessory(self.hass, user_input)
+                await accessory.connect()
+                return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+            except Exception:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception")
+                errors["base"] = "cannot_connect"
 
-        data_schema = vol.Schema({
-            vol.Required("host"): str,
-            vol.Optional("port", default=4096): int,
-            vol.Required("name"): str,
-        })
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_HOST): str,
+                    vol.Required(CONF_NAME): str,
+                    vol.Optional(CONF_PORT, default=DEFAULT_PORT): int,
+                }
+            ),
+            errors=errors,
+        )
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Return the options flow for this handler."""
-        return JRPanelOptionsFlowHandler(config_entry)
+    async def async_step_zeroconf(self, discovery_info):
+        """Handle zeroconf discovery."""
+        _LOGGER.debug("Discovered JR Touch Panel via zeroconf: %s", discovery_info)
 
-class JRPanelOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle JR Panel options."""
+        host = discovery_info.host
+        name = discovery_info.name
+        port = discovery_info.port or DEFAULT_PORT
+        unique_id = discovery_info.hostname
 
-    def __init__(self, config_entry):
-        """Initialize JR Panel options flow."""
-        self.config_entry = config_entry
+        await self.async_set_unique_id(unique_id)
+        self._abort_if_unique_id_configured()
 
-    async def async_step_init(self, user_input=None):
-        """Manage the options."""
-        return self.async_show_form(step_id="init", data_schema=vol.Schema({}))
+        self.context["title_placeholders"] = {"name": name}
+
+        return await self.async_step_user(
+            user_input={CONF_HOST: host, CONF_NAME: name, CONF_PORT: port}
+        )
