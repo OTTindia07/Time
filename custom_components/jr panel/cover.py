05d8cc2916794ae3636fac5c0e97b1dcf01f5f7c
OTTindia07 <meetzankhariyaking@gmail.com>
2024-09-18 22:27:05 +0530

    Update cover.py

--- custom_components/jr panel/cover.py
+++ custom_components/jr panel/cover.py
@@ -1,24 +1,37 @@
-"""Cover service for JR Touch Panel."""
 from homeassistant.components.cover import CoverEntity
+from .tcp_client import JRTouchPanelTCPClient
 
-from .abstract_service import AbstractService
+class JRTouchPanelCover(CoverEntity):
+    """Representation of a motorized cover controlled by the JR Touch Panel."""
 
-class JRCover(CoverEntity, AbstractService):
-    """Representation of a JR Touch Panel cover."""
+    def __init__(self, client: JRTouchPanelTCPClient, device_id: int, name: str):
+        self.client = client
+        self.device_id = device_id
+        self._name = name
+        self._position = 0
 
     @property
-    def is_closed(self):
-        """Return true if the cover is closed."""
-        return self.accessory.entities[self.dp_id]["value"] == 0
+    def name(self):
+        return self._name
 
-    async def async_close_cover(self, **kwargs):
-        """Close the cover."""
-        await self.set_state(0)
+    @property
+    def current_cover_position(self):
+        return self._position
 
     async def async_open_cover(self, **kwargs):
         """Open the cover."""
-        await self.set_state(100)
+        await self.client.send({"device_id": self.device_id, "action": "open"})
+        self._position = 100
+        self.async_write_ha_state()
 
-    async def update(self):
-        """Update the cover state."""
-        self.accessory.entities[self.dp_id]["value"] = await self.accessory.get_state(self.dp_id)
+    async def async_close_cover(self, **kwargs):
+        """Close the cover."""
+        await self.client.send({"device_id": self.device_id, "action": "close"})
+        self._position = 0
+        self.async_write_ha_state()
+
+    async def async_set_cover_position(self, position, **kwargs):
+        """Set the cover to a specific position."""
+        await self.client.send({"device_id": self.device_id, "position": position})
+        self._position = position
+        self.async_write_ha_state()
