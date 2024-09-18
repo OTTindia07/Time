65eeea79d2d51a88223c8d3c27ed4a33c59b76af
OTTindia07 <meetzankhariyaking@gmail.com>
2024-09-18 22:25:36 +0530

    Update switch.py

--- custom_components/jr panel/switch.py
+++ custom_components/jr panel/switch.py
@@ -1,24 +1,31 @@
-"""Switch service for JR Touch Panel."""
 from homeassistant.components.switch import SwitchEntity
+from .tcp_client import JRTouchPanelTCPClient
 
-from .abstract_service import AbstractService
+class JRTouchPanelSwitch(SwitchEntity):
+    """Representation of a switch controlled by the JR Touch Panel."""
 
-class JRSwitch(SwitchEntity, AbstractService):
-    """Representation of a JR Touch Panel switch."""
+    def __init__(self, client: JRTouchPanelTCPClient, device_id: int, name: str):
+        self.client = client
+        self.device_id = device_id
+        self._name = name
+        self._state = False
+
+    @property
+    def name(self):
+        return self._name
 
     @property
     def is_on(self):
-        """Return true if the switch is on."""
-        return self.accessory.entities[self.dp_id]["value"]
+        return self._state
 
     async def async_turn_on(self, **kwargs):
         """Turn the switch on."""
-        await self.set_state(True)
+        await self.client.send({"device_id": self.device_id, "action": "on"})
+        self._state = True
+        self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
         """Turn the switch off."""
-        await self.set_state(False)
-
-    async def update(self):
-        """Update the switch state."""
-        self.accessory.entities[self.dp_id]["value"] = await self.accessory.get_state(self.dp_id)
+        await self.client.send({"device_id": self.device_id, "action": "off"})
+        self._state = False
+        self.async_write_ha_state()
